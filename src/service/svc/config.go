f519e1f01fbf4e7b57ee5cd3f72aa4da6f782d59
conry <kequan888@gmail.com>
2025-08-28 21:15:50 +0800

    init

---
+++ src/service/svc/config.go
@@ -0,0 +1,51 @@
+package svc
+
+import (
+	"github.com/falconry9527/EasySwapBase/evm/erc"
+	//"github.com/falconry9527/EasySwapBase/image"
+	"github.com/falconry9527/EasySwapBase/stores/xkv"
+	"gorm.io/gorm"
+
+	"github.com/falconry9527/EasySwapBackend/src/dao"
+)
+
+type CtxConfig struct {
+	db *gorm.DB
+	//imageMgr image.ImageManager
+	dao     *dao.Dao
+	KvStore *xkv.Store
+	Evm     erc.Erc
+}
+
+type CtxOption func(conf *CtxConfig)
+
+func NewServerCtx(options ...CtxOption) *ServerCtx {
+	c := &CtxConfig{}
+	for _, opt := range options {
+		opt(c)
+	}
+	return &ServerCtx{
+		DB: c.db,
+		//ImageMgr: c.imageMgr,
+		KvStore: c.KvStore,
+		Dao:     c.dao,
+	}
+}
+
+func WithKv(kv *xkv.Store) CtxOption {
+	return func(conf *CtxConfig) {
+		conf.KvStore = kv
+	}
+}
+
+func WithDB(db *gorm.DB) CtxOption {
+	return func(conf *CtxConfig) {
+		conf.db = db
+	}
+}
+
+func WithDao(dao *dao.Dao) CtxOption {
+	return func(conf *CtxConfig) {
+		conf.dao = dao
+	}
+}
